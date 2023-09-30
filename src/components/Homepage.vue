69893c42ccca862e53cea5b74d5ac9f4e6e02f2c
6310210304 <6310210304@psu.ac.th>
2023-09-30 14:11:31 +0700

    edit homepage

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,176 +1,250 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://ced.sci.psu.ac.th/img/ced1.JPG"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      คณะวิทยาศาสตร์
-    </v-card-title>
-
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <!-- <v-btn
-        color="orange lighten-2"
-        text
+  <v-row>
+    <v-col cols="3">
+      <v-card
+        class="mx-auto"
+        max-width="344"
       >
-        Explore
-      </v-btn> -->
+        <v-img 
+          src="https://ced.sci.psu.ac.th/img/ced1.JPG"
+          height="200px"
+        ></v-img>
 
-      <v-spacer></v-spacer>
+        <v-card-title>
+          คณะวิทยาศาสตร์
+        </v-card-title>
 
-      <v-btn
-        icon
-        @click="show = !show"
+        <v-card-subtitle>
+          มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่
+        </v-card-subtitle>
+
+        <!-- <v-card-actions>
+          <v-btn
+            color="orange lighten-2"
+            text
+          >
+            Explore
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+            icon
+            @click="show = !show"
+          >
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
+
+            <v-card-text>
+              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </div>
+        </v-expand-transition> -->
+      </v-card>
+    </v-col>
+ 
+    <v-col cols="3">
+      <v-card
+        class="mx-auto"
+        max-width="344"
       >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
+        <v-img 
+          src="https://shorturl.asia/u8Cty"
+          height="200px"
+        ></v-img>
 
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
+        <v-card-title>
+          คณะวิศวกรรมศาสตร์
+        </v-card-title>
 
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-        </v-card-text>
-      </div>
-    </v-expand-transition>
+        <v-card-subtitle>
+          มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่
+        </v-card-subtitle>
 
+        <!-- <v-card-actions>
+          <v-btn
+            color="orange lighten-2"
+            text
+          >
+            Explore
+          </v-btn>
 
-    <v-img
-      src="https://shorturl.asia/u8Cty"
-      height="200px"
-    ></v-img>
+          <v-spacer></v-spacer>
 
-    <v-card-title>
-      คณะวิศวกรรมศาสตร์
-    </v-card-title>
+          <v-btn
+            icon
+            @click="show = !show"
+          >
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
 
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
 
-    <v-card-actions>
-      <!-- <v-btn
-        color="orange lighten-2"
-        text
+            <v-card-text>
+              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </div>
+        </v-expand-transition> -->
+      </v-card>
+    </v-col>
+
+    <v-col cols="3">
+      <v-card
+        class="mx-auto"
+        max-width="344"
       >
-        Explore
-      </v-btn> -->
+        <v-img 
+          src="https://shorturl.asia/Oat5E"
+          height="200px"
+        ></v-img>
 
-      <v-spacer></v-spacer>
+        <v-card-title>
+          คณะศิลปศาสตร์
+        </v-card-title>
 
-      <v-btn
-        icon
-        @click="show = !show"
+        <v-card-subtitle>
+          มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่
+        </v-card-subtitle>
+
+        <!-- <v-card-actions>
+          <v-btn
+            color="orange lighten-2"
+            text
+          >
+            Explore
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+            icon
+            @click="show = !show"
+          >
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
+
+            <v-card-text>
+              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </div>
+        </v-expand-transition> -->
+      </v-card>
+    </v-col>   
+
+    <v-col cols="3">
+      <v-card
+        class="mx-auto"
+        max-width="344"
       >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
+        <v-img 
+          src="https://f.ptcdn.info/867/048/000/okei8m6mdpAXohH5Cdh-o.jpg"
+          height="200px"
+        ></v-img>
 
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
+        <v-card-title>
+          คณะนิติศาสตร์
+        </v-card-title>
 
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-        </v-card-text>
-      </div>
-    </v-expand-transition>
+        <v-card-subtitle>
+          มหาวิทยาลัยสงขลานครินทร์วิทยาเขตหาดใหญ่
+        </v-card-subtitle>
+
+        <!-- <v-card-actions>
+          <v-btn
+            color="orange lighten-2"
+            text
+          >
+            Explore
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+            icon
+            @click="show = !show"
+          >
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
+
+            <v-card-text>
+              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </div>
+        </v-expand-transition> -->
+      </v-card>
+    </v-col>
 
 
-    <v-img
-      src="https://shorturl.asia/Oat5E"
-      height="200px"
-    ></v-img>
 
-    <v-card-title>
-      คณะศิลปศาสตร์
-    </v-card-title>
 
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <!-- <v-btn
-        color="orange lighten-2"
-        text
+    <v-col cols="3">
+      <v-card
+        class="mx-auto"
+        max-width="344"
       >
-        Explore
-      </v-btn> -->
+        <v-img 
+          src="https://f.ptcdn.info/867/048/000/okei8m6mdpAXohH5Cdh-o.jpg"
+          height="200px"
+        ></v-img>
 
-      <v-spacer></v-spacer>
+        <v-card-title>
+          คณะนิติศาสตร์
+        </v-card-title>
 
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
+        <v-card-subtitle>
+          มหาวิทยาลัยสงขลานครินทร์วิทยาเขตหาดใหญ่
+        </v-card-subtitle>
 
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
+        <!-- <v-card-actions>
+          <v-btn
+            color="orange lighten-2"
+            text
+          >
+            Explore
+          </v-btn>
 
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-        </v-card-text>
-      </div>
-    </v-expand-transition>
+          <v-spacer></v-spacer>
 
+          <v-btn
+            icon
+            @click="show = !show"
+          >
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
 
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
 
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
-
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <!-- <v-btn
-        color="orange lighten-2"
-        text
-      >
-        Explore
-      </v-btn> -->
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
-
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
-
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
+            <v-card-text>
+              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </div>
+        </v-expand-transition> -->
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+
 
 
 
