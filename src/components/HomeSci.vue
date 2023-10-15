924e018342283c32adef8b3f9e42f18dc491ee7b
6310210304 <6310210304@psu.ac.th>
2023-10-15 22:28:39 +0700

    Backend Homepage Homsci Success

--- src/components/HomeSci.vue
+++ src/components/HomeSci.vue
@@ -1,168 +1,55 @@
 <template>
-  <v-row>
-    <v-col cols="4">
-      <v-card
-        class="mx-auto"
-        max-width="344"
-      >
+  <v-container class="mt-10 flex-wrap" >
+    <v-row>
+      <v-col class="col-md-4" v-for="(card, index) in cards" :key="index" cols="12" md="4">
+        <v-card
+          class="mx-auto"
+          max-width="344"
+        >
         <v-img 
-          src="https://www.hatyaifocus.com/ckeditor/upload/forums/1/%E0%B9%82%E0%B8%9A%E0%B9%89/%E0%B8%AA%E0%B8%84./%E0%B9%82%E0%B8%A3%E0%B8%87%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3%E0%B8%9F%E0%B8%B4%E0%B8%AA%E0%B8%B4%E0%B8%81/118175476_3263028410441578_2379294325614708343_o.jpg"
+          :src="Image(card.locationPicture)" 
           height="200px"
-        ></v-img>
-
-        <v-card-title>
-          ตึกฟิสิกส์
+        >
+        </v-img>
+        <v-card-title
+          class="text-center d-flex justify-center align-center "
+        >
+          {{ card.locationName }}
         </v-card-title>
-
-        <v-card-subtitle>
-          เรียกได้ว่าเป็น "โรงอาหารคณะวิทยาศาสตร์" ร้านอาหารต่างๆขายช่วงเช้าถึงบ่ายโมง
-          <br>
-          <br>
+        <v-card-subtitle
+        >
+          {{ card.locationDescription }}
         </v-card-subtitle>
-
-        <!-- <v-card-actions>
-          <v-btn
-            color="orange lighten-2"
-            text
-          >
-            Explore
-          </v-btn>
-
-          <v-spacer></v-spacer>
-
-          <v-btn
-            icon
-            @click="show = !show"
-          >
-            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-
-        <v-expand-transition>
-          <div v-show="show">
-            <v-divider></v-divider>
-
-            <v-card-text>
-              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-            </v-card-text>
-          </div>
-        </v-expand-transition> -->
-      </v-card>
-    </v-col>
- 
-    <v-col cols="4">
-      <v-card
-        class="mx-auto"
-        max-width="344"
-      >
-        <v-img 
-          src="https://www.sci.psu.ac.th/wp-content/uploads/2022/06/5-places-to-chill-out-3.jpg"
-          height="200px"
-        ></v-img>
-
-        <v-card-title>
-          ตึกBSc
-        </v-card-title>
-
-        <v-card-subtitle>
-          มีเซเว่น ร้านขายข้าวไข่เจียว และร้านไก่ย่าง5ดาว ก่อนเรียนหรือเรียนเสร็จนักศึกษามักจะมานั่งรับประทานอาหารกัน
-        </v-card-subtitle>
-
-        <!-- <v-card-actions>
-          <v-btn
-            color="orange lighten-2"
-            text
-          >
-            Explore
-          </v-btn>
-
-          <v-spacer></v-spacer>
-
-          <v-btn
-            icon
-            @click="show = !show"
-          >
-            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-
-        <v-expand-transition>
-          <div v-show="show">
-            <v-divider></v-divider>
-
-            <v-card-text>
-              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-            </v-card-text>
-          </div>
-        </v-expand-transition> -->
-      </v-card>
-    </v-col>
-
-    <v-col cols="4">
-      <v-card
-        class="mx-auto"
-        max-width="344"
-      >
-        <v-img 
-          src="https://scontent-kul2-2.xx.fbcdn.net/v/t1.6435-9/165662959_2806995216296697_5027621551341281790_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFEjn0PsWTIw_urIoiE0dN5A3pFiNAh0e4DekWI0CHR7l1UG9XBou2d10MxAnMP1zuzHYf-yeDomU1VEBjqRRau&_nc_ohc=Jp2oiXCPD2IAX-39uwT&_nc_ht=scontent-kul2-2.xx&oh=00_AfD9Df0wUEQ0U0XRq7rOgVCt9l1VY-iAvCTL98TF5RtZOA&oe=653F8FAF"
-          height="200px"
-        ></v-img>
-
-        <v-card-title>
-          ตึกBSc
-        </v-card-title>
-
-        <v-card-subtitle>
-          ร้าน etc. เป็นคาเฟ่เล็กๆ มีอาหาารอร่อยๆหลายเมนู สามารถนั่งรับประทานในร้านเพื่อตากแอร์เย็นๆ หรือนั่งข้างนอกเพื่อรับลมธรรมชาติก็ได้
-        </v-card-subtitle>
-
-        <!-- <v-card-actions>
-          <v-btn
-            color="orange lighten-2"
-            text
-          >
-            Explore
-          </v-btn>
-
-          <v-spacer></v-spacer>
-
-          <v-btn
-            icon
-            @click="show = !show"
-          >
-            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-
-        <v-expand-transition>
-          <div v-show="show">
-            <v-divider></v-divider>
-
-            <v-card-text>
-              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-            </v-card-text>
-          </div>
-        </v-expand-transition> -->
-      </v-card>
-    </v-col>   
-  </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-
-
-
 
 
 <script>
 export default {
-  data: () => ({
-    show: false,
-  }),
+  data() {
+    return {
+      cards: [],
+    };
+  },
   methods: {
-      HomeSci () {
-          if (this.$refs.form.validate()) {
-              localStorage.setItem('username', this.name)
-          } 
+    async locationData() {
+      try {
+        const response = await this.axios.get("http://localhost:9000/location");
+        this.cards = response.data;
+      } catch (error) {
+        console.error("Error fetching location data:", error);
       }
-  }
-}
+    },Image(locationPicture) {
+      return `data:image/jpeg;base64,${locationPicture}`;
+    },
+
+  },
+  created() {
+    this.locationData();
+  },
+
+};
 </script>
